3677a2849829f42879dac71fe53db90f341fe6c1
Pramit Kumar Pal <41202066+pramitpal@users.noreply.github.com>
2022-06-15 21:50:21 +0530

    Update eda_bundle.sh

--- eda_bundle.sh
+++ eda_bundle.sh
@@ -7,13 +7,15 @@ cd whyRD_eda_bundle
 #pre-requisite
 echo "installing pre-requisite"
 echo "___________________________________________________"
-sudo apt-get install build-essential clang bison flex adms libomp-dev \
+sudo apt-get install build-essential clang bison flex \
 	libreadline-dev gawk tcl-dev libffi-dev git \
 	graphviz xdot pkg-config python3 libboost-system-dev \
 	libboost-python-dev libboost-filesystem-dev zlib1g-dev \
 	make m4 tcsh csh libx11-dev gperf  tcl8.6-dev tk8.6 tk8.6-dev \
 	libxmp4 libxpm-dev  libxcb1 libcairo2  \
       libxrender-dev libx11-xcb-dev libxaw7-dev freeglut3-dev automake
+sudo apt-get install adms
+sudo apt-get install libomp-dev
 echo "pre-requisite installed"
 echo "_________________________________________________"
 #######################################################
